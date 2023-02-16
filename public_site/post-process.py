ecb33729107f392347471c9ac19a16903b1a7fe4
Eric Ellis Freitag <eef49@cornell.edu>
2023-02-16 17:41:49 -0500

    Set up map and panel, improve processing script
    Co-authored-by: milesvr38 <milesvr38@users.noreply.github.com>
    Co-authored-by: lz533 <lz533@users.noreply.github.com>

--- public_site/post-process.py
+++ public_site/post-process.py
@@ -1,11 +1,9 @@
-# Simple post-processing script to create a clean subset of tree data
-#     - JRz, for personal use and not indended for real-world applications
-
-#  NOTE: This is eliminating valid, useful data in lieu of something easier to visualize
-#        It's possible that this will mask trends, as it is often VERY likely that missing data are non-randomly distributed (e.g. more missing data pre-1955 in this dataset)
-
-
 import csv
+
+date_set = []
+for i in range(100):
+    curr_year = 1913 + i
+    date_set.append(curr_year + "-09-01")
 
 
 def passes_filter(row):
@@ -13,45 +11,28 @@ def passes_filter(row):
 
     if row['Country'] != 'United States':
         return False
-    elif row['dt'] != '2000-01-01':
+    elif row['dt'] in date_set:
         return False
-    # if len(row['qSpecies']) < 3 or 'Tree(s) ::' in row['qSpecies']:
-    #     return False
-
-    # elif len(row['Latitude']) < 2 or len(row['Longitude']) < 2:
-    #     return False
-    # elif len(row['qSiteInfo']) < 1 or row['qSiteInfo'] == ':':
-    #     return False
-    # elif len(row['DBH']) < 1:
-    #     return False
     else:
         return True
-
-    # think about what other filters you could run here...
 
 
 # import and run passes_filter
 data = []
 header = []
 
-# TODO: switch out the input .csv file
-with open('GlobalLandTemperaturesByCity.csv', 'r') as f:
+# switch out the input .csv file
+with open('GlobalLandTemperaturesByState.csv', 'r') as f:
     reader = csv.DictReader(f)
 
     header = reader.fieldnames
     for row in reader:
         if passes_filter(row):
-            # you might consider doing some additional processing here
-
-            # name_arr = row['qSpecies'].split(' :: ')
-            # row['qSpecies'] = name_arr[-1]
 
             data.append(row)
 
-print(len(data))
-
-# TODO: name the output file
-with open('GlobalLandTemperaturesByCity_FILTERED.csv', 'w') as f:
+# name the output file
+with open('States1913and2013.csv', 'w') as f:
     writer = csv.DictWriter(f, fieldnames=header)
     writer.writeheader()
     writer.writerows(data)
